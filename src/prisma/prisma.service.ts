b7d05b52a3d8905e7093461888addf027826b699
gwanhun <leegawnhun@gmail.com>
2023-08-09 16:09:42 +0900

    enableShutdownHooks함수 수정

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -31,7 +31,7 @@ export class PrismaService
   }
 
   async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
+    process.on('beforeExit', async () => {
       await app.close();
     });
   }
